94d5560b8bf8ce15a8c66d80d33e0df2e2ca8bf3
mdArifulIslam12 <mdarif767778@gmail.com>
2023-09-07 20:49:08 +0600

    deploy

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import { CategoryController } from './category.controller';
 
-const router = Router();
+const router = express.Router();
 
 router.post(
   '/create-category',
